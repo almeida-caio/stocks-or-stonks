3e71839c31b55510578ff329fed2d906eb371e53
almeida-caio <78153441+almeida-caio@users.noreply.github.com>
2021-02-04 14:37:16 -0300

    Add files via upload

--- image-bank/plotScript_randomWalks_variant.py
+++ image-bank/plotScript_randomWalks_variant.py
@@ -1,7 +1,8 @@
-### draft - 1d random walk or stock prices? ###
-## https://tradeoptionswithme.com/random-walk-theory/
+### 1d random walk (variant) -- plot generator ###
 import numpy as np
 import matplotlib.pyplot as plt
+
+line_colors = ['#E4C580', '#E862C3']
 
 def length_coin():
 	len_aux = 2.9 * np.random.random_sample() + 0.1
@@ -11,9 +12,12 @@ def angle_coin():
     ang_aux = - np.pi * np.random.random_sample() + (np.pi/2)
     return np.sin(ang_aux)
 
-x = np.arange(0, 800, 1)
+delta = 800
+x = np.arange(0, delta, 1)
 
-for iteration in range(0,1):
+for plot_index in range(0, 1000):
+    f = "0-" + str(plot_index) + ".png"
+
     y = []
     interval_partOne = np.arange(4, 200, 5)
     interval_partTwo = np.arange(250, 3500, 148)
@@ -22,12 +26,16 @@ for iteration in range(0,1):
 
     for item in x:
         value = value + (length_coin() * (angle_coin()/2.))
-        # value = value + (angle_coin() / 2.) # <- or maybe this one!
+        # value = value + (angle_coin() / 2.) # <- or maybe this one?
         y.append(value)
-        
-    plt.plot(x,y)
-    # plt.hist(y, bins = 45)
-    # print(np.mean(y))
-    # plt.yscale('log')
 
-plt.show()
+    plt.plot(x, y, color = line_colors[np.random.randint(0,2)])
+
+    ax = plt.axes()
+    ax.set_facecolor('#163170')
+    plt.xticks(size = 12, color = 'white')
+    plt.yticks(size = 12, color = 'white')
+
+    #plt.grid()
+    plt.savefig(f, extension = 'png', facecolor = '#08114A')
+    ax.cla()
--- image-bank/plotScript_realStocks.py
+++ image-bank/plotScript_realStocks.py
@@ -1,30 +1,58 @@
-### draft - 1d random walk or stock prices? ###
-## https://tradeoptionswithme.com/random-walk-theory/
+### real stock prices -- plot generator ###
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-## working kinda fine for real data 
+from PIL import Image
+from PIL import PngImagePlugin
+
+line_colors = ['#E4C580', '#E862C3']
 strArr = ["AAL", "AAPL", "AMZN", "BABA", "BAC", "F", "FB", "GM", "GME", "GOOG", "INTC", "NFLX", "NOK", "PFE", "SNE", "TSLA", "VALE"]
-s_label = np.random.randint(0, len(strArr))
 
-market_compData = pd.read_csv("stock-data/" + strArr[s_label] + ".csv", sep = ',')
-stock_numpyData = market_compData.to_numpy()
+for plot_index in range(0, 1000):
+	f = "1-" + str(plot_index) + ".png"
+	s_label = np.random.randint(0, len(strArr))
 
-stock_prices = []
-for entry in stock_numpyData:
-    stock_prices.append(entry[1])
+	market_compData = pd.read_csv("stock-data/" + strArr[s_label] + ".csv", sep = ',')
+	stock_numpyData = market_compData.to_numpy()
 
-delta = 800
-zeroS = np.random.randint(0, len(stock_prices) - delta)
+	stock_dates = []
+	stock_prices = []
+	for entry in stock_numpyData:
+		stock_dates.append(entry[0])
+		stock_prices.append(entry[1])
 
-while (stock_prices[zeroS] < 3):
-	zeroS = np.random.randint(0, len(stock_prices) - delta)	
+	delta = 800
+	zeroS = np.random.randint(0, len(stock_prices) - delta)
 
-finalS = zeroS + delta
+	while (stock_prices[zeroS] < 3):
+		zeroS = np.random.randint(0, len(stock_prices) - delta)	
 
-x = np.arange(0, delta)
-y = stock_prices[zeroS:finalS]
+	finalS = zeroS + delta
 
-plt.plot(x,y)
-plt.show()
+	x = np.arange(0, delta)
+	y = stock_prices[zeroS:finalS]
+
+	METADATA = {"ticker":str(strArr[s_label]), "start":str(stock_dates[zeroS]), "end":str(stock_dates[finalS-1])}
+
+	plt.plot(x, y, color = line_colors[np.random.randint(0,2)])
+	ax = plt.axes()
+	ax.set_facecolor('#163170')
+	plt.xticks(size = 12, color = 'white')
+	plt.yticks(size = 12, color = 'white')
+
+	# plt.grid()
+	plt.savefig(f, extension = 'png', facecolor = '#08114A')
+	ax.cla()
+
+	# Saving metadata to the image files
+	im = Image.open(f)
+	meta = PngImagePlugin.PngInfo()
+
+	for entry in METADATA:
+	    meta.add_text(entry, METADATA[entry])
+	im.save(f, "png", pnginfo=meta)
+
+	# debugging reasons
+	# im2 = Image.open(f)
+	# print(im2.info)
